8edbda8b35a6565a45175d0d9c12e177d1574907
wahyupanji <wahyupanji16@gmail.com>
2026-07-13 15:23:53 +0700

    add layer using geomapid api

--- webapp-maplibre/src/main.js
+++ webapp-maplibre/src/main.js
@@ -10,7 +10,7 @@ const map = new Map ({
     container: 'map',
     style: 'https://demotiles.maplibre.org/globe.json',
     center: [106.89 , -6.19],
-    zoom: 3
+    zoom: 11
 })
 
 // const data = {
@@ -36,7 +36,7 @@ map.on("load", () => {
 
   map.addSource('kota', {
     type: 'geojson',
-    data: naturalEarthData
+    data: "https://geoserver.mapid.io/layers_new/get_layer?api_key=e39817e8ae824eac8fe466f38a2e0420&layer_id=6a3006fabccdad7e1b82d757&project_id=6a2ffb666684a940bdb05bb8"
   });
 
   map.addLayer({
